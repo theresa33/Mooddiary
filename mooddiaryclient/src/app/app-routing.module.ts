713baf40a11c3725c62e070a76b8eaae9b1da084
Theresa <theresaneurauter@gmail.com>
2022-02-17 11:48:47 +0100

    FE: setup new routing

--- mooddiaryclient/src/app/app-routing.module.ts
+++ mooddiaryclient/src/app/app-routing.module.ts
@@ -2,32 +2,33 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'entry',
-    // path: 'entry/:id',
-    loadChildren: () => import('./pages/entry/entry.module').then( m => m.EntryPageModule)
-  },
-  {
-    path: 'entry-list',
-    loadChildren: () => import('./pages/entry-list/entry-list.module').then( m => m.EntryListPageModule)
-  },
-  {
-    path: 'dashboard',
-    loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-  },
-  {
-    path: 'medpack',
-    loadChildren: () => import('./pages/medpack/medpack.module').then( m => m.MedpackPageModule)
-  },
+
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  // },
+  // {
+  //   path: '',
+  //   redirectTo: 'home',
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'entry',
+  //   // path: 'entry/:id',
+  //   loadChildren: () => import('./pages/entry/entry.module').then( m => m.EntryPageModule)
+  // },
+  // {
+  //   path: 'entry-list',
+  //   loadChildren: () => import('./pages/entry-list/entry-list.module').then( m => m.EntryListPageModule)
+  // },
+  // {
+  //   path: 'dashboard',
+  //   loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
+  // },
+  // {
+  //   path: 'medpack',
+  //   loadChildren: () => import('./pages/medpack/medpack.module').then( m => m.MedpackPageModule)
+  // },
 ];
 
 @NgModule({
--- mooddiaryclient/src/app/pages/home/home-routing.module.ts
+++ mooddiaryclient/src/app/pages/home/home-routing.module.ts
@@ -6,40 +6,22 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-    // children: [
-    //   {
-    //     path: 'dashboard',
-    //     children: [
-    //       {
-    //         path: '',
-    //         loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardPageModule)
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     path: 'entry',
-    //     children: [
-    //       {
-    //         path: '',
-    //         loadChildren: () => import('../entry/entry.module').then(m => m.EntryPageModule)
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     path: 'medpack',
-    //     children: [
-    //       {
-    //         path: '',
-    //         loadChildren: () => import('../medpack/medpack.module').then(m => m.MedpackPageModule)
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     path:'',
-    //     redirectTo: '/app/home/dashboard',
-    //     pathMatch: 'full'
-    //   }
-    //]
+    children: [
+      {
+        path: '',
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+      },
+    ]
   }
 ];
 
