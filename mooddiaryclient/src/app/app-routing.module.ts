82466764c95c3aa89b3e3ae5893293aded3b15d2
Theresa <theresaneurauter@gmail.com>
2022-04-06 17:40:33 +0200

    FE: delete unused code

--- mooddiaryclient/src/app/app-routing.module.ts
+++ mooddiaryclient/src/app/app-routing.module.ts
@@ -7,32 +7,6 @@ const routes: Routes = [
     loadChildren: () => import('./pages/tablinks/tablinks.module').then(m => m.TablinksPageModule)
   }
 
-  // {
-  //   path: 'home',
-  //   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  // },
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path: 'entry',
-  //   // path: 'entry/:id',
-  //   loadChildren: () => import('./pages/entry/entry.module').then( m => m.EntryPageModule)
-  // },
-  // {
-  //   path: 'entry-list',
-  //   loadChildren: () => import('./pages/entry-list/entry-list.module').then( m => m.EntryListPageModule)
-  // },
-  // {
-  //   path: 'dashboard',
-  //   loadChildren: () => import('./pages/dashboard/dashboard.module').then( m => m.DashboardPageModule)
-  // },
-  // {
-  //   path: 'medpack',
-  //   loadChildren: () => import('./pages/medpack/medpack.module').then( m => m.MedpackPageModule)
-  // },
 ];
 
 @NgModule({
