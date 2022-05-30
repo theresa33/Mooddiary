1613bfa8a817b40ebe4c6b70b881f5f3fc414b46
Theresa <theresaneurauter@gmail.com>
2022-05-30 19:39:39 +0200

    chart zwei (not working)

--- mooddiaryclient/src/app/pages/dashboard/dashboard.page.ts
+++ mooddiaryclient/src/app/pages/dashboard/dashboard.page.ts
@@ -2,6 +2,8 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 import { LoadingController, NavController } from '@ionic/angular';
 import { DataService } from 'src/app/services/data.service';
 import { Chart, ChartConfiguration, LineController, LineElement, PointElement, LinearScale, Title, CategoryScale  } from 'chart.js';
+import 'chartjs-adapter-date-fns';
+import toDate from 'date-fns/toDate';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,12 +13,19 @@ import { Chart, ChartConfiguration, LineController, LineElement, PointElement, L
 export class DashboardPage implements OnInit, AfterViewInit {
 
   @ViewChild('lineCanvas') lineCanvas;
+  @ViewChild('lineCanvas2') lineCanvas2;
 
   lineChart: any;
+  lineChart2: any;
+
   entries: any;
+  dates: any;
 
   public chartLabels: any = [];
   public chartValues: any = [];
+
+  public chartValues2: any = [];
+  public chartLabels2: any = [];
 
   constructor(public data: DataService, private navCtrl: NavController,  public loading: LoadingController,) { }
 
@@ -29,11 +38,20 @@ export class DashboardPage implements OnInit, AfterViewInit {
     };
   }
 
+  public newDate(){
+    this.dates = {
+      created_at: ''
+    };
+  }
+
   ngOnInit() {}
 
   ngAfterViewInit() {
     this.defineChartData();
     this.lineChartMethod();
+
+    this.defineChartDate();
+    this.lineChartMethodtwo();
   }
 
 
@@ -51,8 +69,6 @@ export class DashboardPage implements OnInit, AfterViewInit {
     console.log(this.entries);
     console.log(this.chartLabels);
     })
-
-
 
   }
 
@@ -98,6 +114,75 @@ export class DashboardPage implements OnInit, AfterViewInit {
     });
   }
 
+//--------------------ZWEITES CHART------------------------
+defineChartDate() {
+
+  let k: any;
+
+  this.data.getAllDates().subscribe((res) => {
+    this.dates = res;
+    for (k in this.dates){
+      var point = this.dates[k];
+      this.chartLabels2.push(point.created_at);
+      this.chartValues2.push(point.created_at);
+  }
+  console.log(this.dates);
+  console.log(this.chartLabels2);
+  })
+
+}
+
+lineChartMethodtwo() {
+  Chart.register(LineController, LineElement, PointElement, LinearScale, Title, CategoryScale);
+  this.lineChart2 = new Chart(this.lineCanvas2.nativeElement, {
+    type: 'line',
+    data: {
+      labels: this.chartLabels2,
+      datasets: [
+        {
+          label: 'My Dataset',
+          fill: false,
+          borderColor: 'rgba(255,255,255,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.chartValues2,
+          spanGaps: false,
+        }
+      ]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+          max: 10,
+        },
+        x: {
+          // type: 'time',
+          // time: {
+          //   unit: 'day'
+          // }
+
+        /*   ticks: {
+            callback: function(value) {
+                return new Date(value).toLocaleDateString('de-DE', {month:'short', year:'numeric'});
+            },
+          }, */
+
+
+        }
+
+    }
+  }
+
+  });
+}
 
 
 
