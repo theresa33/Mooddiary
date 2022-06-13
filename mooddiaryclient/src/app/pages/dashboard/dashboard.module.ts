0e1b1eafd85eb67be3ff2c5346afeb903b0f87d5
Theresa <theresaneurauter@gmail.com>
2022-06-13 12:31:25 +0200

    FE: Swiper.js

--- mooddiaryclient/src/app/pages/dashboard/dashboard.module.ts
+++ mooddiaryclient/src/app/pages/dashboard/dashboard.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { DashboardPageRoutingModule } from './dashboard-routing.module';
 
 import { DashboardPage } from './dashboard.page';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   imports: [
@@ -14,6 +15,7 @@ import { DashboardPage } from './dashboard.page';
     FormsModule,
     IonicModule,
     DashboardPageRoutingModule,
+    SwiperModule
   ],
   declarations: [DashboardPage]
 })
